20b31c9eb8e483804a4caecb54866d8f3e1edd95
root <heseneli536@gmail.com>
2025-06-09 09:48:34 +0000

    new commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - The main function
+ * Return: 0 if successful, anything else means error
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
