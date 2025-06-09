e0496f368336fea14d69858eeafaeddf3ee6136b
root <heseneli536@gmail.com>
2025-06-09 09:52:20 +0000

    new commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - The main function
+ * Return: 0 if successful, anything else means error
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
