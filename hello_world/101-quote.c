ada9f00917e9970c0ab4ae817cfb2cd6732b1c8f
root <heseneli536@gmail.com>
2025-06-09 10:38:46 +0000

    new commit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - The main function, print to standard error
+ * Return: 0 if successful, anything else means error
+ */
+int main(void)
+{
+	const char *str = "and that piece of art is"
+		" useful\" - Dora Korpar, 2015-10-19\n";
+	int l = 0;
+
+	while (str[l])
+	{
+		l++;
+	}
+	write(2, str, l);
+	return (1);
+}
