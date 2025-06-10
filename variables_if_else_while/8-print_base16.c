c79fd28d3f9e1cc93f7d45f921f1800b410271a2
root <heseneli536@gmail.com>
2025-06-10 09:14:52 +0000

    new commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function, prints alphabet
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
