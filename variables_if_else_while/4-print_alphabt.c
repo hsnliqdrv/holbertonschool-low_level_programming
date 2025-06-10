2b83c00cbf8d4794e83820b9d407ab29df58a3e3
root <heseneli536@gmail.com>
2025-06-10 09:02:16 +0000

    new commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function, prints alphabet except q and e
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
