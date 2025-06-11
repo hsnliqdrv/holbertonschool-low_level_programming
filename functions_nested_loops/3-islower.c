48a7cbdbca8c15fbbb52fa66a9bff6dcbcd8bdda
root <heseneli536@gmail.com>
2025-06-11 14:22:51 +0000

    new commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercaseness
+ * @c: character to be checked
+ *
+ * Return: 1 if c is lower case
+ *         else zero
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
