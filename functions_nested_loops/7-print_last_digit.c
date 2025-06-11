5b41510adae826d29ae578f32077def5b3361122
root <heseneli536@gmail.com>
2025-06-11 15:39:16 +0000

    new commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-/** print_last_digit - prints last digit
+/**
+ * print_last_digit - prints last digit
  * @n: a number
  *
  * Return: Last digit of n
  */
 int print_last_digit(int n)
 {
-	int k = n % 10;
+	int k = n % 10 > 0 ? n % 10 : -n % 10;
 
-	return (k > 0 ? k : -k);
+	_putchar(k);
+	return (k);
 }
