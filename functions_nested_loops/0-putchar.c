552cfc78cbd6bb64bcaab0775f7b44c52ef46798
root <heseneli536@gmail.com>
2025-06-11 13:51:56 +0000

    new commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,21 +2,21 @@
 
 /**
  * _puts - the puts alternative
- * @s - the pointer to the first character of char array (string)
+ * @s: the pointer to the first character of char array (string)
  *
  * Return: Number of characters printed
  */
-int _puts(char* s)
+int _puts(char *s)
 {
 	int l = 0;
-	
-	while(s[l])
+
+	while (s[l])
 	{
 		_putchar(s[l]);
 		l++;
 	}
 	_putchar('\n');
-	return (l+1);
+	return (l + 1);
 }
 /**
  * main - main function
