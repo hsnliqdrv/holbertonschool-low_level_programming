08d4a7edbce07053121493d9554d49118c33d638
root <heseneli536@gmail.com>
2025-06-11 15:50:29 +0000

    new commit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add and return the result
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return sum;
+}
