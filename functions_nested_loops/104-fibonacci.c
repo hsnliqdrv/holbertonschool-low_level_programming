bb0fc33e7b2639d4cf1fae825a9cdad4ead434c4
root <heseneli536@gmail.com>
2025-06-12 10:22:21 +0000

    new

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,11 +2,42 @@
 #include <stdio.h>
 
 /**
- * main - main
+ * first_digit - returns first digit of n
+ * @n: a number
+ *
+ * Return: first digit of n
+ */
+int first_digit(int n)
+{
+	while (n >= 10)
+	{
+		n /= 10;
+	}
+	return (n);
+}
+
+/**
+ * main - the main function, prints first 98 fibo starting with 1 or 2
  * Return: 0
  */
 int main(void)
 {
-	printf("hello");
+	int i = 0, k;
+	unsigned long int a = 0, b = 1;
+
+	while (i < 98)
+	{
+		k = first_digit(a);
+		if (k == 1 || k == 2 || 1)
+		{
+			i++;
+			if (i == 98)
+				printf("%lu\n", a + b);
+			else
+				printf("%lu, ", a + b);
+		}
+		b = a + b;
+		a = b - a;
+	}
 	return (0);
 }
