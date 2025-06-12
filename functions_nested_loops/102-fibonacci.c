dfdfd8b27992113eaa324355b0554c3b96249bae
root <heseneli536@gmail.com>
2025-06-12 09:06:46 +0000

    new commit

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * first_digit - returns first digit of n
+ * @n: a number
+ *
+ * Return: first digit of n
+ */
+int first_digit(int n)
+{
+	while (n >= 10)
+	{
+		n /= 10;
+	}
+	return (n);
+}
+
+/**
+ * main - the main function, prints first 50 fibo starting with 1 or 2
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0, k;
+	int a = 0, b = 1;
+
+	while (i < 50)
+	{
+		k = first_digit(a);
+		if (k == 1 || k == 2)
+		{
+			i++;
+			if (i == 50)
+				printf("%d\n", a);
+			else
+				printf("%d, ", a);
+		}
+		b = a + b;
+		a = b - a;
+	}
+	return (0);
+}
