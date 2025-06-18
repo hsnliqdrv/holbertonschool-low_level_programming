2d10d60f45e7ae44d6f3c44434dadc352a41b2ca
Hasanali Qadirov <heseneli536@gmail.com>
2025-06-18 19:07:51 +0400

    new commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints arrays elements
+ * @a: array
+ * @n: num of elems to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[n - 1]);
+}
