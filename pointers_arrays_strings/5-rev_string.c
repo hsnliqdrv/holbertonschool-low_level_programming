60897935e8d1fff7decf34b141ef26b5a5294d5d
Hasanali Qadirov <heseneli536@gmail.com>
2025-06-18 18:22:14 +0400

    new commit

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i, l = 0;
+	char t;
+
+	while (s[l])
+		l++;
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[l-i-1];
+		s[l-i-1] = t;
+	}
+}
