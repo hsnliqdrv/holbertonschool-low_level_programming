096971d727622fbf3e1665513dc80c06b7e09454
Hasanali Qadirov <heseneli536@gmail.com>
2025-06-18 18:45:37 +0400

    new commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, l = 0;
+
+	while (str[l])
+		l++;
+	for (i = l / 2; i < l; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
