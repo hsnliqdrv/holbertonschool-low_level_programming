02d6d8cd36428bb38c8dd8f4c9039123090bd8f4
hsnliqdrv <heseneli536@gmail.com>
2025-06-20 12:11:58 +0400

    new commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, r = 0;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = r ? '\0' : src[i];
 		if (src[i] == '\0')
-			break;
+			r = 1;
 	}
 	return (dest);
 }
