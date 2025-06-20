dc7fbe61f7b13967dfe9706802c0d0a767a72a83
hsnliqdrv <heseneli536@gmail.com>
2025-06-20 13:23:56 +0400

    new commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * issep - checks if its a seperator
+ * @c: character
+ *
+ * Return: 1 if sep else 0
+ */
+int issep(char c)
+{
+	char *s = " \t\n,;.!?\"(){}";
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalizes string
+ * @s: string
+ *
+ * Return: new string
+ */
+char *cap_string(char *s)
+{
+	int i = 0, wassep = 1;
+
+	while (s[i])
+	{
+		if ((wassep) && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+		if (s[i] == '\t')
+			s[i] = ' ';
+		wassep = issep(s[i]);
+		i++;
+	}
+	return (s);
+}
