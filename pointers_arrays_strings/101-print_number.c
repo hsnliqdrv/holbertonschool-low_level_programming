14309db6256028e4c57abd6b89f893ba266dce25
hsnliqdrv <heseneli536@gmail.com>
2025-06-22 11:01:46 +0400

    new commit

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - prints an int
  * @n: integer
  */
 void print_number(int n)
 {
-	int d = 1000000000, s = 0, k;
+	int d = 1000000000, s = 0, k, sign = 1;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		d = -d;
+		sign = -sign;
 	}
 	if (n == 0)
 	{
@@ -19,8 +19,8 @@ void print_number(int n)
 		return;
 	}
 	while (d != 0)
-	{
-		k = n / d % 10;
+	{ 
+		k = sign * (n / d % 10);
 		if (k == 0)
 		{
 			if (s)
