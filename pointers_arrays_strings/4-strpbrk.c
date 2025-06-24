40018be66a7b290e312e83f179711df4b3c5288d
hsnliqdrv <heseneli536@gmail.com>
2025-06-24 19:14:34 +0400

    new commit

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * contains - if string cont a char
+ * @s: string
+ * @c: char
+ *
+ * Return: 1 if contains else 0
+ */
+int contains(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s++ == c)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _strpbrk - find first occurence of any character
+ * specified in second string in first string
+ * @s: first string
+ * @accept: second string
+ *
+ * Return: pointer to first occurence
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		if (contains(accept, *s++))
+			return (s - 1);
+	}
+	return (s);
+}
