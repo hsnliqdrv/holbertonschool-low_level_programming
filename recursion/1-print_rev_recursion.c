b842ff909a0e98bda213b946c1b4775aadd4dd29
hsnliqdrv <heseneli536@gmail.com>
2025-06-25 15:24:51 +0400

    new commit

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print stirng in reverse
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1))
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
