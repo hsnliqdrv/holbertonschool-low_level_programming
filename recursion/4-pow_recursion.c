df4266693bda0c3f8dc14a114bef88da4c4395b0
hsnliqdrv <heseneli536@gmail.com>
2025-06-25 15:51:25 +0400

    new commit

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power function
+ * @x: first operand
+ * @y: second operand
+ *
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return -1;
+	if (y == 0)
+		return 1;
+	return (x * _pow_recursion(x, y - 1));
+}
