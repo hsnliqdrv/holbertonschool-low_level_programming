652ebf6bddd8bf6b21695efee8fa32f0a861fb6d
hsnliqdrv <heseneli536@gmail.com>
2025-06-26 11:44:47 +0400

    new commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument list
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	puts(argv[0]);
+	return (0);
+}
