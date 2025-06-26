ddeb18fab063ae33d7b83f97c67bc6e02e95ef04
hsnliqdrv <heseneli536@gmail.com>
2025-06-26 11:48:52 +0400

    new commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: argument count
+ * @argv: argument list
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
