714361757f3edb9308f60118c53cda8e3e497954
hsnliqdrv <heseneli536@gmail.com>
2025-06-28 19:39:37 +0400

    new commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,5 +8,21 @@
  */
 int _sqrt_recursion(int n)
 {
+	static int guess = 0;
 
+	if (n < 0)
+		return -1;
+	if (guess * guess > n)
+	{
+		guess = 0;
+		return -1;
+	}
+	if (guess * guess == n)
+	{
+		int result = guess;
+		guess = 0;
+		return result;
+	}
+	guess++;
+	return _sqrt_recursion(n);
 }
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - if input is prime
+ * @n: number
+ *
+ * Return: 1 if prime else 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return 0;
+
+}
