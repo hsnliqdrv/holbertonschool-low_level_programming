d3b1ec2efa216be3a52bf98aaba0425c8e3164ff
hsnliqdrv <heseneli536@gmail.com>
2025-06-28 20:13:30 +0400

    new commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * prime_helper - help primes
+ * @n: number
+ * @d: divider
+ *
+ * Return: 1 if prime else 0
+ */
+int prime_helper(int n, int d)
+{
+	if (d >= n / 2)
+		return (1);
+	if (n % d == 0)
+		return (0);
+	return (prime_helper(n, d + 1));
+}
+/**
  * is_prime_number - if input is prime
  * @n: number
  *
@@ -8,7 +23,7 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 1)
-		return 0;
-
+	if (n <= 1)
+		return (0);
+	return (prime_helper(n, 2));
 }
