81f21bfd83dabeda87f0eb7bf5904a2029da51a6
hsnliqdrv <heseneli536@gmail.com>
2025-06-30 17:51:10 +0400

    new commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,19 +10,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char* ptr;
+	char *ptr;
+	unsigned int i;
 
 	if (size == 0)
-		return NULL;
-	else
-	{
-		unsigned int i;
+		return (NULL);
 
-		ptr = malloc(size);
-		if (!ptr)
-			return NULL;
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
-		return ptr;
-	}
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
