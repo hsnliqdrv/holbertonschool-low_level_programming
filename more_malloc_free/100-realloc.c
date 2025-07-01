6440b47987b8faf1e0e15863680a13e8dc427b73
hsnliqdrv <heseneli536@gmail.com>
2025-07-01 20:03:26 +0400

    new commit

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - realloc alternative
+ * @ptr: pointer to previously allocated mem
+ * @old_size: size of memory allocated previously
+ * @new_size: new size of memory block at ptr
+ *
+ * Return: if new_size == 0, NULL
+ * else pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int copy, i;
+	char *new;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	copy = new_size < old_size ? new_size : old_size;
+	new = malloc(new_size);
+	if (!new)
+		return (NULL);
+	for (i = 0; i < copy; i++)
+		new[i] = ((char *) ptr)[i];
+	free(ptr);
+	return ((void *) new);
+}
