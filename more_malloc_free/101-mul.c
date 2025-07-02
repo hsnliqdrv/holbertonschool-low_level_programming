08adea8c2013d5e0e5668d5125306488652fcee1
hsnliqdrv <heseneli536@gmail.com>
2025-07-02 20:21:56 +0400

    new commit

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,5 +1,42 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+
+/**
+ * print - prints string
+ * @s: string
+ */
+void print(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		_putchar(s[i++]);
+}
+/**
+ * _strdup - duplicate given string
+ * @str: string
+ *
+ * Return: if str = null, null
+ * if insufficient memory, null
+ * if successful, pointer to duplicate
+ */
+char *_strdup(char *str)
+{
+	unsigned int l = 0, i;
+	char *ptr;
+
+	if (!str)
+		return (NULL);
+	while (str[l])
+		l++;
+	ptr = malloc(l + 1);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		ptr[i] = str[i];
+	ptr[l] = '\0';
+	return (ptr);
+}
 /**
  * len - length of string
  * @s: string
@@ -18,8 +55,10 @@ int len(char *s)
  * add_zeros - addes zeros to end of string
  * @s: string
  * @z: number of zeros
+ *
+ * Return: new string
  */
-void add_zeros(char *s, int z)
+char *add_zeros(char *s, int z)
 {
 	int ls = len(s);
 	int l = ls + z, i;
@@ -28,62 +67,157 @@ void add_zeros(char *s, int z)
 	for (i = ls; i < l; i++)
 		s[i] = '0';
 	s[l] = '\0';
+	return (s);
 }
+
+/**
+ * zero - zeroes memory
+ * @s: string
+ * @size: number of bytes
+ */
+void zero(char *s, int size)
+{
+	int i = 0;
+
+	while (i < size)
+		s[i++] = '0';
+}
+
 /**
  * add - addition
  * @n1: first number
  * @n2: second number
- * @r: result
- * @size_r: result size
  *
  * Return: resulting number
  */
-char *add(char *n1, char *n2, char *r, int size_r)
+char *add(char *n1, char *n2)
 {
 	int d = 0, a, b, i, j;
 	int l1 = len(n1), l2 = len(n2);
-	char *f;
+	int size = l1 > l2 ? l1 + 1 : l2 + 1;
+	char *f = malloc(size + 1), *s;
 
-	r[size_r - 1] = '\0';
-	for (i = 1; i < size_r; i++)
+	f[size] = '\0';
+	zero(f, size);
+	for (i = 1; i < size + 1; i++)
 	{
 		j = i - 1;
 		a = l1 - j >= 1 ? n1[l1 - j - 1] - '0' : 0;
 		b = l2 - j >= 1 ? n2[l2 - j - 1] - '0' : 0;
-		r[size_r - i - 1] = '0' + (a + b + d) % 10;
+		f[size - i] = '0' + (a + b + d) % 10;
 		d = (a + b + d) / 10;
 	}
-	j = i - 1;
-	a = l1 - j >= 1 ? n1[l1 - j - 1] - '0' : 0;
-	b = l2 - j >= 1 ? n2[l2 - j - 1] - '0' : 0;
-	if (a + b + d > 0)
-		return (0);
-	f = r;
-	while (*f == '0' && f < r + size_r - 2)
-		f++;
+	if (*f == '0')
+	{
+		s = _strdup(f + 1);
+		free(f);
+		f = s;
+	}
 	return (f);
 }
 
 /**
- * _mul - multiplication of first opreand of 
+ * _mul - multiplication of first opreand of
  * any length and second operand only one digit
  * @s: string of digits
  * @d: a digit
- * 
+ *
  * Return: result of multiplication
  */
+
 char *_mul(char *s, char d)
 {
-	int n = d - '0', i, ls = len(s);
-	char *r = malloc(ls + 2);
-	for (i = 0; i < ls + 1; i++)
-		r[i] = '0';
-	r[ls + 1] = '\0';
-	for (i = 0; i < d; i++)
+	int n = d - '0', i;
+	char *r, *ss;
+
+	r = malloc(2);
+	r[0] = '0';
+	r[1] = '\0';
+
+	for (i = 0; i < n; i++)
 	{
-		r = add(r, s, r, ls + 2);
-		if (!r)
+		ss = add(r, s);
+		if (!ss)
 			return (NULL);
+		if (i > 0)
+			free(r);
+		r = ss;
 	}
-	return r;
+	return (r);
+}
+
+/**
+ * mul - multiplies numbers
+ * @s1: first
+ * @s2: second
+ *
+ * Return: s1 * s2
+ */
+char *mul(char *s1, char *s2)
+{
+	int l2 = len(s2), i;
+	char *r = "0", *a, *b, *c;
+
+	for (i = l2 - 1; i >= 0; i--)
+	{
+		a = _mul(s1, s2[i]);
+		b = add_zeros(a, l2 - i - 1);
+		c = add(r, b);
+		free(b);
+		if (i < l2 - 1)
+		{
+			free(r);
+		}
+		r = c;
+	}
+
+	return (r);
+}
+
+/**
+ * isonlydigits - checks if string is only digits
+ * @s: string
+ *
+ * Return: if only digits, 1. Else, 0
+ */
+int isonlydigits(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument list
+ *
+ * Return: if successful, 0
+ * else, 98
+ */
+int main(int argc, char **argv)
+{
+	char *s;
+
+	if (argc != 3)
+	{
+		print("Error\n");
+		return (98);
+	}
+	if (!(isonlydigits(argv[1]) && isonlydigits(argv[2])))
+	{
+		print("Error\n");
+		return (98);
+	}
+	s = mul(argv[1], argv[2]);
+	print(s);
+	_putchar('\n');
+	free(s);
+	return (0);
 }
