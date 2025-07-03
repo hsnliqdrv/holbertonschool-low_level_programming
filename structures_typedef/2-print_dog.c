9cdb28aeb1daa81ccf763fc6ea3ddd9bbc6bb914
hsnliqdrv <heseneli536@gmail.com>
2025-07-03 15:11:29 +0400

    new commit

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog
+ * @d: pointer to dog
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: ");
+	if (d -> name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d -> name);
+	printf("Age: %f\n", d -> age);
+	printf("Owner: ");
+	if (d -> owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d -> owner);
+}
