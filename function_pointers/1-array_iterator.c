11f265a18cc2278e7eca7017bbfc84d6495857b3
hsnliqdrv <heseneli536@gmail.com>
2025-07-07 17:06:52 +0400

    new commit

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute function on each element
+ * @array: array
+ * @size: size of array
+ * @action: function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
