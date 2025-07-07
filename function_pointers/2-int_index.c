cb32c93d2116f35df39860affee505f4c5ab5071
hsnliqdrv <heseneli536@gmail.com>
2025-07-07 17:18:48 +0400

    new commit

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * int_index - searches index
  * @array: array
@@ -9,6 +10,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
