0e926228e6c7dc04f7ba7c61ceb0b1583b42c036
hsnliqdrv <heseneli536@gmail.com>
2025-07-08 14:25:38 +0400

    new commit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers witha seperator
+ * @separator: seperates numbers
+ * @n: number of args
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
