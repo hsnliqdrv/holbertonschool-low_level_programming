c536c3121ed79d79a4105137e3c879829a36d473
hsnliqdrv <heseneli536@gmail.com>
2025-07-08 15:38:41 +0400

    new commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0, c = 0;
+	va_list list;
+
+	va_start(list, format);
+}
