d6fb847b061b78cfdce68c32af2d3380825be97d
hsnliqdrv <heseneli536@gmail.com>
2025-07-21 18:55:58 +0400

    new commit

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 __attribute__((constructor))
-void init()
+/**
+ * init - init function
+ */
+void init(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
