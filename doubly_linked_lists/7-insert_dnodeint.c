b8fbfe88cf6f3348c842a66f7ffe9a4ffb7bfb40
hsnliqdrv <heseneli536@gmail.com>
2025-07-22 19:01:01 +0400

    new commit

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,12 @@
+/**
+ * insert_dnodeint_at_index - insert a node to index
+ * @h: head of list
+ * @idx: index
+ * @n: integer
+ *
+ * Return: new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	while ()
+}
