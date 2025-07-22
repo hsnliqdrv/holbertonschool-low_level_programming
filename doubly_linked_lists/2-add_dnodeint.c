c26d7e15610787843351a5a8c492057eacdee328
hsnliqdrv <heseneli536@gmail.com>
2025-07-22 18:01:29 +0400

    new commit

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add node to beginning of list
+ * @head: pointer to head of list
+ * @n: integer
+ *
+ * Return: pointer to new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
