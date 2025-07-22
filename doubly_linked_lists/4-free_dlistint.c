d96f61a0de661441c208e9da2d4047f863d6ba55
hsnliqdrv <heseneli536@gmail.com>
2025-07-22 18:34:06 +0400

    new commit

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t* tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
