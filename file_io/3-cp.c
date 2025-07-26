76f3523bcada2e65a800e041a829ce1260ea9928
Hasanali Qadirov <heseneli536@gmail.com>
2025-07-26 19:36:38 +0400

    new commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -65,6 +65,8 @@ int main(int argc, char **argv)
 			error_98(argv[1]);
 		if (r == 0)
 			break;
+		if (r < 1024 && read(fds, buffer, 1024) != 0)
+			error_98(argv[1]);
 		w = write(fdd, buffer, (size_t) r);
 		if (w < r)
 			error_99(argv[2]);
