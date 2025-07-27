f531c49104ae42d54cc6faab43c891e5ed6f8789
Hasanali Qadirov <heseneli536@gmail.com>
2025-07-27 17:14:34 +0400

    new commit

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get a value associated with a key
+ * @ht: hash table
+ * @key: the key
+ *
+ * Return: value if key exists, else NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	const unsigned char *ukey = (const unsigned char *) key;
+	hash_node_t *node = find(ht->array[key_index(ukey, ht->size)], key);
+
+	return (node ? node->value : NULL);
+}
