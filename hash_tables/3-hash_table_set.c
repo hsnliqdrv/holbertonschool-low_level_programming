8809012cb9270ed049d6d38a99494f7822ed94e6
Hasanali Qadirov <heseneli536@gmail.com>
2025-07-27 17:01:42 +0400

    new commit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,6 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * find - check if key exists in hash node list
+ * @hnl: head of hash node list
+ * @key: the key
+ *
+ * Return: if not existing NULL, else pointer to node
+ */
+hash_node_t *find(hash_node_t *hnl, const char *key)
+{
+	if (!hnl)
+		return (NULL);
+	if (strcmp(hnl->key, key) == 0)
+		return (hnl);
+	return (find(hnl->next, key));
+}
+/**
  * hash_table_set - add an element to hash table
  * @ht: hash table
  * @key: key for element
@@ -13,26 +28,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node, *it;
 	unsigned long int ind;
+	char *dk, *dv;
 
 	if (strlen(key) == 0 || !key || !value || !ht || !ht->array)
 		return (0);
+	ind = key_index((const unsigned char *) key, ht->size);
+	it = find(ht->array[ind], key);
+	dv = strdup(value);
+	if (!dv)
+		return (0);
+	if (it)
+	{
+		free(it->value);
+		it->value = dv;
+		return (1);
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-	node->key = strdup(key);
-	node->value = strdup(value);
-	node->next = NULL;
-	if (!node->value || !node->key)
-		return (0);
-	ind = key_index((const unsigned char *) key, ht->size);
-	it = ht->array[ind];
-	if (!it)
+	dk = strdup(key);
+	if (!dk)
 	{
-		ht->array[ind] = node;
-		return (1);
+		free(dv);
+		free(node);
+		return (0);
 	}
-	while (it->next)
-		it = it->next;
-	it->next = node;
+	node->key = dk;
+	node->value = dv;
+	node->next = ht->array[ind];
+	ht->array[ind] = node;
 	return (1);
 }
